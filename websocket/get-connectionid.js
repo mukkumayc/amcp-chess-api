dcdfb9cc465ce9e464a29c3c2e434f859fa559cc
MukkuMayc <st063775@student.spbu.ru>
2019-12-16 23:08:18 +0300

    Fix errors

--- websocket/get-connectionid.js
+++ websocket/get-connectionid.js
@@ -1,4 +1,5 @@
 import { success } from '../libs/response-lib';
+import AWS from "aws-sdk";
 
 export async function main(event, context) {
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
@@ -6,7 +7,7 @@ export async function main(event, context) {
     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage,
   });
   await apigwManagementApi.postToConnection({
-    ConnectionId: result.Item.connectionId1,
+    ConnectionId: event.requestContext.connectionId,
     Data: JSON.stringify({
       action: "getConnectionId",
       connectionId: event.requestContext.connectionId,
