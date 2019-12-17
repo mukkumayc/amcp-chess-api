cf9be2616d09848c06789c8696a4931453b932f3
MukkuMayc <st063775@student.spbu.ru>
2019-12-17 11:45:57 +0300

    Add notations

---
+++ generators/notations.js
@@ -0,0 +1,8 @@
+let notations = [
+    "1. Nf3 Nf6 2. g3 b6 3. Bg2 Bb7 4. O-O c5 5. d3 e6 6. e4 d6 7. Nc3 Be7",
+    "1. c4 Nf6 2. d4 c6 3. Nf3 d5 4. e3 a6 5. b3 Bg4 6. Nbd2 Nbd7 7. Be2 e6 8. O-O Bd6 9. Bb2 Qc7 10. c5 Be7 11. b4 O-O 12. Qc2 Bf5 13. Bd3 Bxd3 14. Qxd3 Rfe8",
+    "1. d4 Nf6 2. Bf4 c5 3. dxc5 Na6 4. Qd4 Qa5+ 5. Nc3 Nxc5 6. Bd2 Ne6 7. Qd3 Qd8",
+    "1. e4 c5 2. Nc3 e6 3. d3 d5 4. g3 Nf6 5. Bg2 Be7 6. f4 Nc6 7. Nge2 Rb8",
+    "1. d4 d5 2. c4 e6 3. Nf3 Nf6 4. g3 Nbd7 5. Bg2 Bd6 6. O-O O-O 7. Nbd2 c6 8. Qc2 Re8 9. e4 dxe4 10. Nxe4 Nxe4 11. Qxe4 e5 12. Rd1 Qc7 13. Be3 Nf6 14. Qc2 Bg4 15. c5 Bf8 16. dxe5 Nd7 17. Ng5 g6 18. f3 Bf5 19. Ne4 Qxe5 20. b4 Rad8 21. Bd4 Bxe4",
+    "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bf4 c5 5. e3 cxd4 6. exd4 Be7 7. Nf3 O-O 8. h3 a6"
+]
