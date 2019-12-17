78ef82f470f5d6d131d6cc07955624933d53392c
MikhailBekhovskiy <mbexovskij@gmail.com>
2019-12-17 18:53:54 +0300

    +

--- addToUsers.js
+++ addToUsers.js
@@ -1,11 +1,11 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import { failure } from "./libs/response-lib";
 
 export async function main(event, context){
     const params = {
         TableName: process.env.UsersTableName,
         userId: event.requestContext.identity.cognitoIdentityId,
-    }
+    };
     try{
         await dynamoDbLib.call("put", params);
         return {
