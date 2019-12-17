754ef50c3fe4a14063db2803bfcc35e635698a78
MikhailBekhovskiy <mbexovskij@gmail.com>
2019-12-17 11:49:13 +0300

    examples added

--- generators/notations.js
+++ generators/notations.js
@@ -4,5 +4,11 @@ let notations = [
     "1. d4 Nf6 2. Bf4 c5 3. dxc5 Na6 4. Qd4 Qa5+ 5. Nc3 Nxc5 6. Bd2 Ne6 7. Qd3 Qd8",
     "1. e4 c5 2. Nc3 e6 3. d3 d5 4. g3 Nf6 5. Bg2 Be7 6. f4 Nc6 7. Nge2 Rb8",
     "1. d4 d5 2. c4 e6 3. Nf3 Nf6 4. g3 Nbd7 5. Bg2 Bd6 6. O-O O-O 7. Nbd2 c6 8. Qc2 Re8 9. e4 dxe4 10. Nxe4 Nxe4 11. Qxe4 e5 12. Rd1 Qc7 13. Be3 Nf6 14. Qc2 Bg4 15. c5 Bf8 16. dxe5 Nd7 17. Ng5 g6 18. f3 Bf5 19. Ne4 Qxe5 20. b4 Rad8 21. Bd4 Bxe4",
-    "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bf4 c5 5. e3 cxd4 6. exd4 Be7 7. Nf3 O-O 8. h3 a6"
+    "1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bf4 c5 5. e3 cxd4 6. exd4 Be7 7. Nf3 O-O 8. h3 a6",
+    "1. Nf3 Nf6 2.b3 g6 3.Bb2 Bg7 4.g3 d6 5.Bg2 O-O 6.O-O c6 7.d3 e5 8.c4 Ne8 9.Nbd2 f5 10.Qc2 Na6 11.c5 Nxc5 12.Nxe5 Qe7 13.d4 Na6 14.Qc4+ Kh8 15.Nef3 Be6 16.Qc3 f4 17.gxf4 Rxf4 18.Qe3 Rf8 19.Ng5 Nec7 20.Nc4 Rae8 21.Nxe6 Qxe6 22.Qxe6 Rxe6",
+    "1. d4 Nf6 2.Nd2 d5 3.Ngf3 e6 4.e3 c5 5.c3 Nbd7 6.Bd3 Bd6",
+    "1. c4 c5 2.Nc3 Nf6 3.Nf3 d5 4.cxd5 Nxd5 5.e4 Nb4 6.b3",
+    "1. e4 c5 2.Nf3 e6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 d6 6.Be2",
+    "1. d4 Nf6 2.c4 c5 3.d5 e6 4.Nc3 exd5 5.cxd5 d6 6.Nf3 g6",
+    "1. c4 e5 2.g3 Nc6 3.Bg2 f5 4.e3 Nf6 5.Ne2 Be7 6.O-O O-O"
 ]
