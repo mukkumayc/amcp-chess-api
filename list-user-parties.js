018101e719d068f12cf965f8a1fdd54e9812b06e
MukkuMayc <st063775@student.spbu.ru>
2019-12-17 06:37:30 +0300

    Add list user's parties method

---
+++ list-user-parties.js
@@ -0,0 +1,29 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.GamesArchiveTableName,
+    // ConditionalOperator: "OR",
+    // KeyConditionExpression: "user1Id = :userId or user2Id = :userId",
+    // QueryFilter: {
+    //     "user1Id": {
+    //         "ConditionalOperator": "OR",
+    //     }
+    //     "user1Id = :userId or user2Id = :userId",
+    // }
+    FilterExpression: "user1Id = :userId or user2Id = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    // Return the list of items in response body
+    return success(result.Items);
+  } catch (e) {
+      console.log(e);
+    return failure({ status: false });
+  }
+}
