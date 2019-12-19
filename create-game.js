c08e3485ab012b23ef3b8221a3a2c0e354ac836f
MukkuMayc <st063775@student.spbu.ru>
2019-12-19 10:48:05 +0300

    Add isHidden parameter

--- create-game.js
+++ create-game.js
@@ -3,13 +3,26 @@ import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
 
 export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  let isHidden;
+  switch (data.roomType) {
+    case "open": 
+      isHidden = false;
+      break;
+    case "hidden":
+      isHidden = true;
+      break;
+    default:
+      console.log("Unknown room type", data.roomType);
+      return failure();
+  }
   const params = {
-    TableName: process.env.OpenRoomsTableName,
+    TableName: process.env.RoomsTableName,
     Item: {
       gameId: uuid.v1(),
-      // playerId1: event.requestContext.identity.cognitoIdentityId,
       createdAt: Date.now(),
       isStarted: false,
+      isHidden: isHidden,
     }
   };
 
