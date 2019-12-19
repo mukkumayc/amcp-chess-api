c4cdd656610af62b605a7bdc7931550dff4330ea
MukkuMayc <st063775@student.spbu.ru>
2019-12-19 08:37:41 +0300

    Update sendIdentity

--- sendIdentity.js
+++ sendIdentity.js
@@ -1,41 +1,47 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context){
-    const data = JSON.parse(event.body);
-    let params = {
-        TableName: process.env.OpenRoomsTableName,
-        Key:{
-            gameId: data.gameId,
-        }
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  let params = {
+    TableName: process.env.OpenRoomsTableName,
+    Key:{
+        gameId: data.gameId,
+    }
+  };
+  const result = await dynamoDbLib.call("get", params);
+  if (result.Item) {
+    let updateExpression;
+    if (!result.Item.playerId1) {
+      updateExpression = "SET playerId1 = :playerId";
+    }
+    else if (!result.Item.playerId2) {
+      updateExpression = "SET playerId2 = :playerId";
+    }
+    else {
+      return success({text: "Room is full"});
+    }
+    params = {
+      TableName: process.env.OpenRoomsTableName,
+      Key: {
+        gameId: data.gameId,
+      },
+      UpdateExpression: updateExpression,
+      ExpressionAttributeValues: {
+        ":playerId": event.requestContext.identity.cognitoIdentityId,
+      },
     };
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item){
-        if (result.Item.playerId1 != event.requestContext.identity.cognitoIdentityId && !result.Item.playerId2){
-            let updateExpression = "SET playerId2 = :playerId";
-            params = {
-                TableName: process.env.OpenRoomsTableName,
-                Key: {
-                    gameId: data.gameId,
-                },
-                UpdateExpression: updateExpression,
-                ExpressionAttributeValues:{
-                    ":playerId": event.requestContext.identity.cognitoIdentityId,
-                },
-            };
-            try{
-                await dynamoDbLib.call("update", params);
-                return success();
-            }catch(e){
-                console.log("error: ", e);
-                console.log("event:", event);
-                return failure({status: false});
-            }
-        }
-        return success();
+    try {
+      await dynamoDbLib.call("update", params);
+    } catch(e) {
+      console.log("error: ", e);
+      console.log("event:", event);
+      return failure({status: false});
     }
-    else{
-        console.log("Item not found");
-        return failure({status:false, error:"Item not found"});
-    }
+    return success();
+  }
+  else {
+    console.log("Item not found");
+    return failure({status:false, error:"Item not found"});
+  }
 }
