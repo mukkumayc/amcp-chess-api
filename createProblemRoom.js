262b5c03e3e4c113ef45a5ad55d9088a9df31451
MukkuMayc <st063775@student.spbu.ru>
2019-12-22 12:46:30 +0300

    Add createProblemRoom function

---
+++ createProblemRoom.js
@@ -0,0 +1,27 @@
+import uuid from 'uuid';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.RoomsTableName,
+    Item: {
+      gameId: uuid.v1(),
+      createdAt: Date.now(),
+      playerId: event.requestContext.identity.cognitoIdentityId,
+      isStarted: true,
+      isHidden: true,
+      problemId: data.problemId,
+    }
+  };
+
+  try {
+    await dynamoDbLib.call('put', params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
